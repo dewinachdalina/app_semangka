5805b63b3c82b8faac12b1afc0a3274d6012ebb2
dewinachdalina <dewi.n.achdalina@gmail.com>
2023-06-05 15:39:37 +0700

    new

--- lib/CameraPagematang.dart
+++ lib/CameraPagematang.dart
@@ -16,41 +16,41 @@ class CameraPageKematangan extends StatefulWidget {
 
 class _CameraPageKematanganState extends State<CameraPageKematangan> {
   String result = "";
-  late CameraController _cameraController;
+  CameraController? _cameraController;
   late CameraImage _cameraImage;
   bool isWorking = false;
   bool _isRearCameraSelected = true;
 
   loadModel() async {
     await Tflite.loadModel(
-      model: "kematangbest-fp16.tflite",
-      labels: "kematang.txt",
+      model: 'assets/kematangbest-fp16.tflite',
+      labels: 'assetskematang.txt',
     );
   }
 
-  initCamera(CameraDescription cameraDescription) {
+  initCamera(CameraDescription cameraDescription) async {
+    _cameraController?.dispose(); // Dispose the previous controller if exists
+
     _cameraController =
-        CameraController(widget.cameras[0], ResolutionPreset.high);
-    _cameraController.initialize().then((cameras) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {
-        _cameraController.startImageStream((imageFromStream) => {
-              if (!isWorking)
-                {
-                  isWorking = true,
-                  _cameraImage = imageFromStream,
-                  runModelOnStreamFrames(),
-                }
-            });
+        CameraController(cameraDescription, ResolutionPreset.high);
+
+    try {
+      await _cameraController!.initialize();
+      setState(() {});
+      _cameraController!.startImageStream((imageFromStream) {
+        if (!isWorking) {
+          isWorking = true;
+          _cameraImage = imageFromStream;
+          runModelOnStreamFrames();
+        }
       });
-    });
+    } catch (e) {
+      print('Error initializing camera: $e');
+    }
   }
 
   runModelOnStreamFrames() async {
-    // ignore: unnecessary_null_comparison
-    if (_cameraImage != null) {
+    if (_cameraImage.planes.isNotEmpty) {
       var recognitions = await Tflite.runModelOnFrame(
         bytesList: _cameraImage.planes.map((plane) {
           return plane.bytes;
@@ -69,7 +69,7 @@ class _CameraPageKematanganState extends State<CameraPageKematangan> {
       recognitions?.forEach((response) {
         result += response["label"] +
             " " +
-            (response["cofidence"] as double).toStringAsFixed(2) +
+            (response["confidence"] as double).toStringAsFixed(2) +
             "\n\n";
       });
       setState(() {
@@ -83,33 +83,38 @@ class _CameraPageKematanganState extends State<CameraPageKematangan> {
   void initState() {
     super.initState();
     loadModel();
+    if (widget.cameras.isNotEmpty) {
+      initCamera(widget.cameras[0]);
+    }
   }
 
   @override
   void dispose() async {
     super.dispose();
+    await _cameraController?.dispose();
     await Tflite.close();
-    _cameraController.dispose();
   }
 
-  Future takePicture() async {
-    if (!_cameraController.value.isInitialized) {
+  Future<void> takePicture() async {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
       return null;
     }
-    if (_cameraController.value.isTakingPicture) {
+    if (_cameraController!.value.isTakingPicture) {
       return null;
     }
     try {
-      await _cameraController.setFlashMode(FlashMode.off);
-      XFile picture = await _cameraController.takePicture();
+      await _cameraController!.setFlashMode(FlashMode.off);
+      final XFile picture = await _cameraController!.takePicture();
       Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => PreviewPage(
-                    picture: picture,
-                  )));
-    } on CameraException catch (e) {
-      debugPrint('Error occured while taking picture: $e');
+        context,
+        MaterialPageRoute(
+          builder: (context) => PreviewPage(
+            picture: picture,
+          ),
+        ),
+      );
+    } catch (e) {
+      debugPrint('Error occurred while taking a picture: $e');
       return null;
     }
   }
@@ -117,49 +122,62 @@ class _CameraPageKematanganState extends State<CameraPageKematangan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Stack(children: [
-        (_cameraController.value.isInitialized)
-            ? CameraPreview(_cameraController)
-            : Container(
-                color: Colors.black,
-                child: const Center(child: CircularProgressIndicator())),
-        Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: const BoxDecoration(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            _cameraController != null && _cameraController!.value.isInitialized
+                ? CameraPreview(_cameraController!)
+                : Container(
+                    color: Colors.black,
+                    child: const Center(child: CircularProgressIndicator()),
+                  ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.20,
+                decoration: const BoxDecoration(
                   borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-                  color: Colors.black),
-              child:
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                Expanded(
-                    child: IconButton(
-                  padding: EdgeInsets.zero,
-                  iconSize: 30,
-                  icon: Icon(
-                      _isRearCameraSelected
-                          ? CupertinoIcons.switch_camera
-                          : CupertinoIcons.switch_camera_solid,
-                      color: Colors.white),
-                  onPressed: () {
-                    setState(
-                        () => _isRearCameraSelected = !_isRearCameraSelected);
-                    initCamera(widget.cameras[_isRearCameraSelected ? 0 : 1]);
-                  },
-                )),
-                Expanded(
-                    child: IconButton(
-                  onPressed: takePicture,
-                  iconSize: 50,
-                  padding: EdgeInsets.zero,
-                  constraints: const BoxConstraints(),
-                  icon: const Icon(Icons.circle, color: Colors.white),
-                )),
-                const Spacer(),
-              ]),
-            )),
-      ]),
-    ));
+                  color: Colors.black,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        iconSize: 30,
+                        icon: Icon(
+                          _isRearCameraSelected
+                              ? CupertinoIcons.switch_camera
+                              : CupertinoIcons.switch_camera_solid,
+                          color: Colors.white,
+                        ),
+                        onPressed: () async {
+                          setState(() {
+                            _isRearCameraSelected = !_isRearCameraSelected;
+                          });
+                          await initCamera(
+                              widget.cameras[_isRearCameraSelected ? 0 : 1]);
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: takePicture,
+                        iconSize: 50,
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        icon: const Icon(Icons.circle, color: Colors.white),
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
